e840cab0501d14a6f3d7b2e8667b0f4ff28a30e1
YunDoll <mar0722@naver.com>
2024-05-17 13:47:55 +0900

    05/17

---
+++ src/db.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+
+mongoose.connect("//127.0.0.1:27017/wetube");
--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const handleHome = (req, res, next) => {
-  return res.send("I love middleware");
-};
-
-app.get("/", handleHome);
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log("Server listening on port http://localhost:4000 🚀");
